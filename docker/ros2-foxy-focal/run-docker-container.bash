5b02392196c71c5494eb1201e052cce71a2c0e7f
chikuta <takeshi.chiku@gmail.com>
2020-09-21 01:45:13 +0900

    Support ros2 focal (this dockerfile uses minimum components)

---
+++ docker/ros2-foxy-focal/run-docker-container.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# start sharing xhost
+xhost +local:root
+user=`id -un`
+
+# run docker
+docker run --rm \
+  --net=host \
+  --ipc=host \
+  --gpus all \
+  --privileged \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:$docker/.Xauthority \
+  -v $HOME/work:$HOME/work \
+  -e XAUTHORITY=$home_folder/.Xauthority \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -it --name "ros-foxy" ${user}/ros-foxy-focal
