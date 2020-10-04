440e8132a902a246297182c7fe7063b7893f1c8c
chikuta <takeshi.chiku@gmail.com>
2020-10-04 18:20:31 +0900

    Add ros-bionic-melodic-unity dockerfile

---
+++ docker/ros-bionic-melodic-unity/build-dokcer-image.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file_dir=`dirname $0`
+
+# get parameter from system
+user=`id -un`
+group=`id -gn`
+uid=`id -u`
+gid=`id -g`
+
+# build docker images
+docker build -t ${user}/ros-bionic-melodic-unity \
+    --build-arg USER=${user} \
+    --build-arg UID=${uid} \
+    --build-arg GROUP=${group} \
+    --build-arg GID=${gid} \
+    ${file_dir}
