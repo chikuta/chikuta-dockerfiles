4c2b42aafcedb56e42f049d3f0f6f368e14803d8
chikuta <takeshi.chiku@gmail.com>
2020-10-05 00:49:54 +0900

    Fix user name

--- docker/ros-bionic-melodic/run-docker-container.bash
+++ docker/ros-bionic-melodic/run-docker-container.bash
@@ -18,4 +18,4 @@ docker run --rm \
   -e XAUTHORITY=$home_folder/.Xauthority \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
-  -it --name "ros-melodic" chikuta/ros-bionic-melodic
+  -it --name "ros-melodic" ${user}/ros-bionic-melodic
