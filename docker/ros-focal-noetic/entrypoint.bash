61d26e009c07f89e8559e2535c51edf9aef9ba72
chikuta <takeshi.chiku@gmail.com>
2021-09-22 23:15:26 +0900

    Support ros-focal-noetic

---
+++ docker/ros-focal-noetic/entrypoint.bash
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+source /opt/ros/noetic/setup.bash
+exec $@
---
+++ docker/ros-focal-noetic/run-docker-container.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get parameter from system
+user=`id -un`
+
+# start sharing xhost
+xhost +local:root
+
+# run docker
+docker run --rm \
+  --net=host \
+  --ipc=host \
+  --gpus all \
+  --privileged \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:$docker/.Xauthority \
+  -v $HOME/work:$HOME/work \
+  -e XAUTHORITY=$home_folder/.Xauthority \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -it --name "ros-noetic" ${user}/ros-focal-noetic
